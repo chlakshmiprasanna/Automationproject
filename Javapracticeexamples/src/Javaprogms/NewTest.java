ad5f2600804c370581270a5fde5d2a99bda0c482
lakshmiprasanna <prasanna.chavakula@gmail.com>
2018-06-19 16:18:18 +0530

    changes to the sysout stmt

--- Javapracticeexamples/src/Javaprogms/NewTest.java
+++ Javapracticeexamples/src/Javaprogms/NewTest.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("First testcase using testng");
+	  System.out.println("First testcase using testng and also trying with github and command line");
   }
 }
